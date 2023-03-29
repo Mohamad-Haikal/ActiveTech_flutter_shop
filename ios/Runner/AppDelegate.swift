bce72893cec8d21943a0693a4664ee1e67f4e5a0
Mohamad Haikal <mohamad.m.haikal0@gmail.com>
2023-03-29 08:01:59 +0300

    new edits

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,19 +3,36 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    if (!isInDebugMode) {
-      // In production mode, log to an external service.
-      // For example, Firebase Crashlytics.
-      GeneratedPluginRegistrant.register(with: self)
-    } else {
-      // In development mode, log to the console.
-      GeneratedPluginRegistrant.register(with: self)
-      FlutterDebugPlugin.sharedInstance().startObservingFlutterLifecycleEvents()
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "samples.flutter.dev/battery", binaryMessenger: controller.binaryMessenger)
+        channel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            guard call.method == "getBatteryLevel" else {
+                result(FlutterMethodNotImplemented)
+                return
+            }
+            self?.getBatteryLevel(result: result)
+        })
+        GeneratedPluginRegistrant.register(with: self)
+        // Enable debugging mode
+        #if DEBUG
+        FlutterDebugPlugin.sharedInstance()?.startObservingApplication()
+        #endif
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    private func getBatteryLevel(result: FlutterResult) {
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        if device.batteryState == UIDevice.BatteryState.unknown {
+            result(FlutterError(code: "UNAVAILABLE",
+                                message: "Battery info unavailable",
+                                details: nil))
+        } else {
+            result(Int(device.batteryLevel * 100))
+        }
+    }
 }
