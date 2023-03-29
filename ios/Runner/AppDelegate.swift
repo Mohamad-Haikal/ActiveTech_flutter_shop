4837bbe43087d07780f17e7c9af2977e8d9d0fc7
Mohamad Haikal <mohamad.m.haikal0@gmail.com>
2023-03-29 08:56:09 +0300

    new edits

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,20 @@
 import UIKit
 import Flutter
+import FirebaseCrashlytics
+import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
+@objc class AppDelegate: FlutterAppDelegate, CrashlyticsDelegate {
+    
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        Fabric.with([Crashlytics.self])
+        
+        Crashlytics.sharedInstance().delegate = self
+        Crashlytics.sharedInstance().crash()
+        
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         let channel = FlutterMethodChannel(name: "samples.flutter.dev/battery", binaryMessenger: controller.binaryMessenger)
         channel.setMethodCallHandler({
@@ -18,12 +26,15 @@ import Flutter
             self?.getBatteryLevel(result: result)
         })
         GeneratedPluginRegistrant.register(with: self)
+
         // Enable debugging mode
         #if DEBUG
         FlutterDebugPlugin.sharedInstance()?.startObservingApplication()
         #endif
+        print("Application launched")
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
+    
     private func getBatteryLevel(result: FlutterResult) {
         let device = UIDevice.current
         device.isBatteryMonitoringEnabled = true
@@ -34,5 +45,9 @@ import Flutter
         } else {
             result(Int(device.batteryLevel * 100))
         }
+    }
+    
+    func crashlyticsDidDetectReport(forLastExecution report: CLSReport) {
+        print("Crash detected: \(report)")
     }
 }
