0618374a022300289100e58b4e828d037da03385
Mohamad Haikal <mohamad.m.haikal0@gmail.com>
2023-03-29 07:35:14 +0300

    fix app crashes on startup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    if (!isInDebugMode) {
+      // In production mode, log to an external service.
+      // For example, Firebase Crashlytics.
+      GeneratedPluginRegistrant.register(with: self)
+    } else {
+      // In development mode, log to the console.
+      GeneratedPluginRegistrant.register(with: self)
+      FlutterDebugPlugin.sharedInstance().startObservingFlutterLifecycleEvents()
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
