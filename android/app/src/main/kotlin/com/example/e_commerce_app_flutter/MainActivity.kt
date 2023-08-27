046fb8ecd9cc992ff47ad4edb2982016691ccab1
Mohamad Haikal <mohamad.m.haikal0@gmail.com>
2023-08-27 03:51:45 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/e_commerce_app_flutter/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.e_commerce_app_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
